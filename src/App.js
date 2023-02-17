5e588f6dda307b58f3fae501bc4a5550bdc1f672
Mostafa-Mohammedi <121897256+Mostafa-Mohammedi@users.noreply.github.com>
2023-02-17 21:55:32 +0100

    Added the react routing to pur component inside the views folder
    This commit message  added the react routing for routing between the three main components  changes made from Mostafa Mohammed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
+import Login from './views/Login';
+import Main from './views/Main';
+import Profile from './views/Profile';
+
+
+import {BrowserRouter as Routing, Routes, Route} from 'react-router-dom'
 
 function App() {
   console.log( process.env.REACT_APP_API_KEY)
 
   return (
-    <div >
-    </div>
+      <Routing>
+        <div className='App'>
+        <Routes>
+          <Route exact path='/' element={<Login/>}/>
+          <Route path='/main' element={<Main/>}/>
+          <Route path='/profile' element={<Profile/>}/>
+        </Routes>
+        </div>
+      </Routing>
   );
 }
 
